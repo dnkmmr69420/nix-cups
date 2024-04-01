5b26fa9f8106e5e2271351694ac6ad1495fd6c9d
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-04-01 11:21:03 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo -i nix profile install nixpkgs#cups
+
+sudo ln -sf /nix/var/nix/profiles/default/lib/systemd/system/cups-lpd.socket /etc/systemd/system/cups-lpd.socket
+sudo ln -sf /nix/var/nix/profiles/default/lib/systemd/system/cups-lpd@.service /etc/systemd/system/cups-lpd@.service
+sudo ln -sf /nix/var/nix/profiles/default/lib/systemd/system/cups.path /etc/systemd/system/cups.path
+sudo ln -sf /nix/var/nix/profiles/default/lib/systemd/system/cups.service /etc/systemd/system/cups.service
+sudo ln -sf /nix/var/nix/profiles/default/lib/systemd/system/cups.socket /etc/systemd/system/cups.socket
+sudo ln -sf /nix/var/nix/profiles/default/etc/cups /etc/cups
+sudo systemctl enable --now avahi-daemon
+sudo systemctl enable --now cups
+sudo -i nix profile install nixpkgs#system-config-printer
